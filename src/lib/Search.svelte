05d610446530aa526c075a289b30b2e9640ddd34
deozza <edenn.touitou@letsjam.fr>
2024-02-25 19:04:16 +0100

    added id to img and name

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -10,6 +10,6 @@
 </script>
 
 <div class="flex flex-row justify-between">
-    <input type="text" placeholder="Type here" class="input input-bordered w-6/12" bind:value={name} />
-    <button class="btn w-5/12 btn-primary" on:click={redirectAccordingToNameSearch}>Search</button>
+    <input type="text" placeholder="Type here" class="input input-bordered w-6/12" id="input-search" bind:value={name} />
+    <button class="btn w-5/12 btn-primary" id="button-search" on:click={redirectAccordingToNameSearch}>Search</button>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,11 @@
 
 <Search />
 <div class="flex flex-col">
-  {#each pokemons as pokemon}
+  {#each pokemons as pokemon, i}
     <a href="./{pokemon.name}" class="text-white transition ease-in-out delay-150 hover:text-primary">
       <div class="flex flex-row flex-grow justify-between items-center card bordered shadow-lg bg-base-200 my-3 p-3">
-        <img src="{pokemon.sprite}" alt="sprite of {pokemon.name}">
-        <span>{pokemon.name}</span>
+        <img src="{pokemon.sprite}" alt="sprite of {pokemon.name}" id="sprite-{i+1}">
+        <span id="name-{i+1}">{pokemon.name}</span>
       </div>
     </a>
   {/each}
